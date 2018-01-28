7e01a1464a48c92ecdcc5ea618498dd861dfea6b
mrzmmr <mrzmmr@gmail.com>
2018-01-28 01:40:19 +0000

    lint index

--- index.js
+++ index.js
@@ -1,37 +1,37 @@
-'use strict'
+'use strict';
 
-var vfile = require('vfile')
+var vfile = require('vfile');
 
-module.exports = visit
+module.exports = visit;
 
-function visit (file, callback) {
-  var root = vfile({contents: [file]})
-  var list = [file, root]
-  var i
+function visit(file, callback) {
+  var root = vfile({contents: [file]});
+  var list = [file, root];
+  var i;
 
   while (list.length > 0) {
-    var current = list.shift()
-    var parent = list.shift()
+    var current = list.shift();
+    var parent = list.shift();
 
-    var index = parent.contents.indexOf(current)
+    var index = parent.contents.indexOf(current);
 
     if (!(current instanceof vfile)) {
-      current = vfile(current)
+      current = vfile(current);
     }
 
-    parent.contents[index] = current
+    parent.contents[index] = current;
 
     if (typeof callback === 'function') {
-      callback(current, parent, index)
+      callback(current, parent, index);
     }
 
     if (Array.isArray(current.contents)) {
-      i = -1
+      i = -1;
       while (i++ < current.contents.length - 1) {
-        list.push(current.contents[i], current)
+        list.push(current.contents[i], current);
       }
     }
   }
 
-  return root.contents[0]
+  return root.contents[0];
 }
--- test.js
+++ test.js
@@ -1,29 +1,29 @@
-'use strict'
+'use strict';
 
-var vfile = require('vfile')
-var tape = require('tape')
-var visit = require('./')
+var vfile = require('vfile');
+var tape = require('tape');
+var visit = require('./');
 
 tape('vfile-visit', function (t) {
   t.test('converts contents to vfile', function (st) {
-    var foo = vfile({contents: [{path: 'bar', contents: ['hello']}]})
-    foo = visit(foo)
-    st.ok(foo.contents[0] instanceof vfile)
-    st.ok(foo.contents[0].contents[0] instanceof vfile)
-    st.end()
-  })
+    var foo = vfile({contents: [{path: 'bar', contents: ['hello']}]});
+    foo = visit(foo);
+    st.ok(foo.contents[0] instanceof vfile);
+    st.ok(foo.contents[0].contents[0] instanceof vfile);
+    st.end();
+  });
 
   t.test('callback calls on un modified contents', function (st) {
     visit({contents: [{path: 'bar'}]}, function (c, p, i) {
-      st.ok(c instanceof vfile)
+      st.ok(c instanceof vfile);
       if (Array.isArray(c.contents)) {
         c.contents.forEach(function (cc) {
-          st.ok(!(cc instanceof vfile))
-        })
+          st.ok(!(cc instanceof vfile));
+        });
       }
-    })
-    st.end()
-  })
+    });
+    st.end();
+  });
 
-  t.end()
-})
+  t.end();
+});
